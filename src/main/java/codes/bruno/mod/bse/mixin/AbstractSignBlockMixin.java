e710f41985bfcdd11e42078e7a1768d608f52149
Bruno Henrique Paiva <bruno@bruno.codes>
2024-05-31 21:34:19 -0300

    chore: better usage of MixinExtras

--- src/main/java/codes/bruno/mod/bse/mixin/AbstractSignBlockMixin.java
+++ src/main/java/codes/bruno/mod/bse/mixin/AbstractSignBlockMixin.java
@@ -2,19 +2,17 @@ package codes.bruno.mod.bse.mixin;
 
 import codes.bruno.mod.bse.config.ServerConfig;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.block.AbstractSignBlock;
-import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.Hand;
-import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(AbstractSignBlock.class)
 public class AbstractSignBlockMixin {
 
+    @Unique
     private static final ServerConfig serverConfig = ServerConfig.getInstance();
 
     @ModifyExpressionValue(
@@ -24,15 +22,7 @@ public class AbstractSignBlockMixin {
             target = "Lnet/minecraft/block/AbstractSignBlock;isTextLiteralOrEmpty(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/block/entity/SignBlockEntity;Z)Z"
         )
     )
-    public boolean bse$canEditSign(
-        boolean original,
-        BlockState state,
-        World world,
-        BlockPos pos,
-        PlayerEntity player,
-        Hand hand,
-        BlockHitResult hit
-    ) {
+    public boolean bse$canEditSign(boolean original, @Local(argsOnly = true) PlayerEntity player) {
         return original && (!serverConfig.isRequireSneakToEdit() || player.isSneaking());
     }
 
